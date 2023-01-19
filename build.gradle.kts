ae25b3944efb250c2540252c408e4a094830a09a
js <js@cleancode.kr>
2023-01-19 14:14:26 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ configurations {
 	}
 }
 
+
 repositories {
 	mavenCentral()
 //	maven("https://jitpack.io")
--- src/test/java/com/example/project_server110_springboot/projectserver110springbootApplicationTests.java
+++ src/test/java/com/example/project_server110_springboot/projectserver110springbootApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.project_server110_springboot;
 
+import com.example.project_server110_springboot.entity.openapi.Covid19noti;
+import com.example.project_server110_springboot.repository.openapi.Covid19notiRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class projectserver110springbootApplicationTests {
 
+	private Covid19notiRepository covid19notiRepository;
+
+	@Autowired
+	projectserver110springbootApplicationTests(Covid19notiRepository covid19notiRepository) {
+		this.covid19notiRepository = covid19notiRepository;
+
+	}
 	@Test
 	void contextLoads() {
+		covid19notiRepository.save(new Covid19noti());
 	}
 
 }
